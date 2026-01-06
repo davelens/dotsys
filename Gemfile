b5f71a645b656080f9e3e880f4e4560b54242655
davelens <github@davelens.be>
2026-01-06 16:58:29 +0100

    Strip all but the bare minimum from this repo, so I can start migrating the setup scripts from my dotfiles in here.

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rubocop-performance', '~> 1.4.1'
-gem 'rubocop-rails', '~> 2.3.1'
--- install_apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# The helper contains a number of methods to help us install our software
-. helper.sh
-. install_dependencies.sh
-
-echo ""
-echo -n "$(tput setaf 11)Would you like to download additional software?$(tput sgr0) $(tput bold)(y/n)$(tput setaf 11):$(tput sgr0) "
-read install_apps
