fceb446f60dd90e6f480b7f852c699d570491dc5
davelens <github@davelens.be>
2026-01-08 19:57:49 +0100

    arch: Properly install the Albert service.

--- arch/init.d/albert.sh
+++ arch/init.d/albert.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-ln -sf "$DOTSYS_REPO_HOME/arch/systemd/albert.service" \
-  "$XDG_CONFIG_HOME/systemd/user/"
-
 # --mflags --skipinteg: Skip integrity check (upstream checksum sometimes outdated)
 paru -S --needed --noconfirm --mflags --skipinteg albert-bin
 systemctl --user daemon-reload
+systemctl --user enable "$DOTSYS_REPO_HOME/arch/systemd/albert.service"
 systemctl --user restart albert
 
 echo -e "Albert is ready!\n"
