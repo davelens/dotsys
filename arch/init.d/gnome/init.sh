8a65cf6900c232c87127b482e6e7511fda0f0ca9
davelens <github@davelens.be>
2026-01-08 17:15:35 +0100

    arch: Replace a few `utility` commands from my dotfiles with temporary substitutes hosted by this repo.

--- arch/init.d/gnome/init.sh
+++ arch/init.d/gnome/init.sh
@@ -4,7 +4,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "==> Installing GNOME packages..."
-utility arch pacman --install-from-file "$SCRIPT_DIR/../../packages/gnome.txt"
+"$SCRIPT_DIR/../../bin/pacman" --install-from-file "$SCRIPT_DIR/packages/gnome.txt"
 
 echo "==> Configuring GNOME settings..."
 "$SCRIPT_DIR/settings.sh"
