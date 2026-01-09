89543b25660037fa97dbed5ab66e1e60ae8fbdb0
davelens <github@davelens.be>
2026-01-09 16:34:17 +0100

    arch: Make for a better way of enabling all systemd services.

--- arch/init.d/albert.sh
+++ arch/init.d/albert.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # --mflags --skipinteg: Skip integrity check (upstream checksum sometimes outdated)
 paru -S --needed --noconfirm --mflags --skipinteg albert-bin
 systemctl --user daemon-reload
-systemctl --user enable "$DOTSYS_REPO_HOME/arch/systemd/albert.service"
+systemctl --user enable "$SCRIPT_DIR/../systemd/albert.service"
 systemctl --user restart albert
 
 echo -e "Albert is ready!\n"
--- arch/init.d/alt-tab.sh
+++ arch/init.d/alt-tab.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Alt+Tab window switcher for Sway (Gnome-style)
 # https://github.com/lostatc/swtchr
 
 echo "==> Configuring swtchr..."
 
-ln -sf "$DOTSYS_REPO_HOME/arch/systemd/swtchrd.service" \
-  "$XDG_CONFIG_HOME/systemd/user/"
-
 sudo pacman -S --needed --noconfirm gtk4 gtk4-layer-shell
 paru -S --needed --noconfirm swtchr
 systemctl --user daemon-reload
-systemctl --user enable --now swtchrd
+systemctl --user enable "$SCRIPT_DIR/../systemd/swtchrd.service"
+systemctl --user restart swtchrd
