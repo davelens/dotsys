8fec137285e093fe15586b985fd04649c7d1fa37
davelens <github@davelens.be>
2026-02-24 23:06:58 +0100

    arch/init.d: Remove the `gdm` check in greetd.sh.

--- arch/init.d/greetd.sh
+++ arch/init.d/greetd.sh
@@ -5,12 +5,6 @@ echo "==> Setting up greetd + tuigreet..."
 
 sudo pacman -S --needed --noconfirm greetd greetd-tuigreet
 
-# Disable GDM if it's currently enabled.
-if systemctl is-enabled gdm.service &>/dev/null; then
-  echo "    Disabling gdm..."
-  sudo systemctl disable gdm.service
-fi
-
 # Configure greetd to use tuigreet with Sway as the default session.
 sudo mkdir -p /etc/greetd
 sudo tee /etc/greetd/config.toml >/dev/null <<'EOF'
