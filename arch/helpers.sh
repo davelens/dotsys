91fd1446008acb73890fb62d3827be268e608791
davelens <github@davelens.be>
2026-03-01 18:48:38 +0100

    Remove the DOTFILES_REPO_HOME coupling.

--- arch/helpers.sh
+++ arch/helpers.sh
@@ -2,8 +2,7 @@
 command_exists 'read_packages' && return 0
 
 DOTSYS_REPO_HOME="$(cd "$(dirname "$(dirname "${BASH_SOURCE[0]}")")" && pwd)"
-DOTFILES_REPO_HOME="${DOTFILES_REPO_HOME:-$HOME/Repositories/davelens/dotfiles}"
-export DOTSYS_REPO_HOME DOTFILES_REPO_HOME
+export DOTSYS_REPO_HOME
 
 read_packages() {
   grep -v '^\s*#' "$1" | grep -v '^\s*$' | awk '{print $1}'
