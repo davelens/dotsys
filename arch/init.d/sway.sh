f010fec6d7b9dad757ff7e1c8999d0ad2cf1aeaa
davelens <github@davelens.be>
2026-03-04 16:44:55 +0100

    arch/init.d: Change a (commented) reference to the old swaylock config location.

--- arch/init.d/sway.sh
+++ arch/init.d/sway.sh
@@ -17,7 +17,7 @@ paru -S --needed --noconfirm swaylock-effects
 
 # Fingerprint auth with swaylock.
 # TODO: Yeah this isn't ideal. There's a best-practice way to do this instead.
-# sudo cp "$DOTFILES_REPO_HOME/config/arch/swaylock/pam-d.config" /etc/pam.d/swaylock
+# sudo cp "$DOTFILES_REPO_HOME/config/swaylock/pam-d.config" /etc/pam.d/swaylock
 
 mkdir -p "$HOME/Pictures"
 cp -r "$DOTSYS_REPO_HOME/wallpapers" ~/Pictures/
