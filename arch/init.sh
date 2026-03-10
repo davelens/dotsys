49db5cba32247ddc6887de2c80732008c2b56d65
davelens <github@davelens.be>
2026-03-10 18:22:26 +0100

    arch: Disable a bunch of scripts by default to shave off a significant amount of time from the initial installation.

--- arch/init.sh
+++ arch/init.sh
@@ -5,20 +5,23 @@ DOTSYS_REPO_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 "$DOTSYS_REPO_HOME/arch/preflight.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/wifi.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/bluetooth.sh"
-"$DOTSYS_REPO_HOME/shared/gnupg/init.sh"
-"$DOTSYS_REPO_HOME/shared/mise/init.sh"
-"$DOTSYS_REPO_HOME/shared/cargo/init.sh"
-"$DOTSYS_REPO_HOME/shared/gh/init.sh"
-"$DOTSYS_REPO_HOME/arch/packages/init.sh" --update
+"$DOTSYS_REPO_HOME/arch/init.d/sway.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/greetd.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/kanshi.sh"
-"$DOTSYS_REPO_HOME/arch/init.d/sway.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/kanata.sh"
-"$DOTSYS_REPO_HOME/arch/init.d/mariadb.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/power-profiles.sh"
-#"$DOTSYS_REPO_HOME/arch/init.d/waybar.sh"
-#"$DOTSYS_REPO_HOME/arch/init.d/alt-tab.sh"
-#"$DOTSYS_REPO_HOME/shared/postgres/init.sh"
+"$DOTSYS_REPO_HOME/shared/gnupg/init.sh"
+"$DOTSYS_REPO_HOME/shared/gh/init.sh"
+
+# These are disabled because I either don't need them by default anymore,
+# or I want to install them delayed because they eat up too much time.
+# "$DOTSYS_REPO_HOME/shared/mise/init.sh"
+# "$DOTSYS_REPO_HOME/shared/cargo/init.sh"
+# "$DOTSYS_REPO_HOME/arch/init.d/mariadb.sh"
+# "$DOTSYS_REPO_HOME/arch/packages/init.sh" --update
+# "$DOTSYS_REPO_HOME/arch/init.d/waybar.sh"
+# "$DOTSYS_REPO_HOME/arch/init.d/alt-tab.sh"
+# "$DOTSYS_REPO_HOME/shared/postgres/init.sh"
 
 # NOTE: This is disabled for now until I can properly "reset" this to
 # the default state of things. It should by-and-large work though.
