9fbdea3ec5295feb6ffe11da5e97e3065bd3bf8f
davelens <github@davelens.be>
2026-04-15 20:06:20 +0200

    arch/init: Remove the shared/gh/init.sh script, since we removed it.

--- arch/init.sh
+++ arch/init.sh
@@ -11,7 +11,6 @@ DOTSYS_REPO_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 "$DOTSYS_REPO_HOME/arch/init.d/kanata.sh"
 "$DOTSYS_REPO_HOME/arch/init.d/power-profiles.sh"
 "$DOTSYS_REPO_HOME/shared/gnupg/init.sh"
-"$DOTSYS_REPO_HOME/shared/gh/init.sh"
 
 # These are disabled because I either don't need them by default anymore,
 # or I want to install them delayed because they eat up too much time.
@@ -21,6 +20,7 @@ DOTSYS_REPO_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 # "$DOTSYS_REPO_HOME/arch/init.d/postgres.sh"
 # "$DOTSYS_REPO_HOME/arch/packages/init.sh" --update
 # "$DOTSYS_REPO_HOME/arch/init.d/waybar.sh"
+# "$DOTSYS_REPO_HOME/shared/gh/init.sh"
 
 # NOTE: This is disabled for now until I can properly "reset" this to
 # the default state of things. It should by-and-large work though.
