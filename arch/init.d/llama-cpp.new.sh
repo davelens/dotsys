740938356862f5802646ae1141d478f9bfa5d0fb
davelens <github@davelens.be>
2026-04-26 15:54:24 +0200

    arch/init.d: Add a note to `llama-cpp.new.sh` with how to run the server.

--- arch/init.d/llama-cpp.new.sh
+++ arch/init.d/llama-cpp.new.sh
@@ -19,3 +19,21 @@ cd llama.cpp
 rm -rf build
 cmake -B build -DGGML_VULKAN=ON
 cmake --build build -j --target llama-server llama-cli
+
+# Download and build ggerganov/llama.cpp and run the server like so:
+#
+# ./ggerganov/llama.cpp/build/bin/llama-server \
+#   -m ~/Documents/models/qwen/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf \
+#   --alias qwen \
+#   -ngl 999 \
+#   -c 262144 \
+#   -b 256 \
+#   --chat-template-file ./ggerganov/llama.cpp/models/templates/Qwen-Qwen3-0.6B.jinja \
+#   --temp 0.2 \
+#   --top-p 0.9 \
+#   --repeat-penalty 1.1 \
+#   --mlock --mmap \
+#   --host 0.0.0.0 \
+#   --port 8080
+#
+# Host on 0.0.0.0 to make it accessible on my Tailscale network.
